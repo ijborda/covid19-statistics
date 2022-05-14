d92492378f5e4021a3b8c44bc911c638750042e4
Imari Joy Borda <imari.borda2018@gmail.com>
2022-05-14 16:13:13 +0800

    Add options

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@
 
 AOS.init();
 
+fetch('https://covid19.mathdro.id/api/countries')
+    .then(res => res.json())
+    .then(data => {
+        let selection = document.querySelector('#selection')
+        data.countries.forEach(a => {
+            let option = document.createElement('option')
+            option.value = a.name
+            option.innerHTML = a.name
+            selection.appendChild(option)
+        })
+    })
+
 fetch('https://covid19.mathdro.id/api')
     .then(res => res.json())
     .then(current => {
@@ -10,8 +22,8 @@ fetch('https://covid19.mathdro.id/api')
         let confCurrent = current.confirmed.value
         let diedCurrent = current.deaths.value
         let dateCurrent = current.lastUpdate
-        document.querySelector('#worldConf').innerHTML = num(confCurrent)
-        document.querySelector('#worldDied').innerHTML = num(diedCurrent)
+        document.querySelector('#conf').innerHTML = num(confCurrent)
+        document.querySelector('#died').innerHTML = num(diedCurrent)
         document.querySelector('#date').innerHTML = date(current.lastUpdate)
 
         
@@ -95,3 +107,79 @@ function calcDaysAgo(current, num) {
     return new Date(new Date(current) - (num + 1)*24*60*60*1000).toISOString().split('T')[0]
 }
 
+
+document.querySelector('#submit').addEventListener('click', function() {
+    let location = document.querySelector('#selection').value
+
+    fetch(`https://covid19.mathdro.id/api/countries/${location}`)
+    .then(res => res.json())
+    .then(current => {
+        // Get current data
+        let confCurrent = current.confirmed.value
+        let diedCurrent = current.deaths.value
+        let dateCurrent = current.lastUpdate
+        document.querySelector('#conf').innerHTML = num(confCurrent)
+        document.querySelector('#died').innerHTML = num(diedCurrent)
+        document.querySelector('#date').innerHTML = date(current.lastUpdate)
+        document.querySelector('#location').innerHTML = location
+
+        // Initialize fields
+        document.querySelector('#confYesterday').innerHTML = 'Loading...'
+        document.querySelector('#diedYesterday').innerHTML = 'Loading...'
+        document.querySelector('#confWeekago').innerHTML = 'Loading...'
+        document.querySelector('#diedWeekago').innerHTML = 'Loading...'
+        document.querySelector('#confMonthago').innerHTML = 'Loading...'
+        document.querySelector('#diedMonthago').innerHTML = 'Loading...'
+        document.querySelector('#confQuarterago').innerHTML = 'Loading...'
+        document.querySelector('#diedQuarterago').innerHTML = 'Loading...'
+
+        // Get yesterday data
+        let dateYesterday = calcDaysAgo(dateCurrent, 1)
+        fetch(`https://covid19.mathdro.id/api/daily/${dateYesterday}`)
+            .then(res => res.json())
+            .then(yesterday => {
+                yesterday = yesterday.filter(a => a.countryRegion === location)
+                let confYesterday = yesterday.reduce((acc, a) => acc + +a['confirmed'], 0) 
+                let diedYesterday = yesterday.reduce((acc, a) => acc + +a['deaths'], 0) 
+                document.querySelector('#confYesterday').innerHTML = change(confCurrent, confYesterday) + '% (+ ' + diff(confCurrent, confYesterday) + ')'
+                document.querySelector('#diedYesterday').innerHTML = change(diedCurrent, diedYesterday) + '% (+ ' + diff(diedCurrent, diedYesterday) + ')'
+            })
+
+        // Get 7 days ago data
+        let dateWeekago = calcDaysAgo(dateCurrent, 7)
+        fetch(`https://covid19.mathdro.id/api/daily/${dateWeekago}`)
+            .then(res => res.json())
+            .then(weekago => {
+                weekago = weekago.filter(a => a.countryRegion === location)
+                let confWeekago = weekago.reduce((acc, a) => acc + +a['confirmed'], 0) 
+                let diedWeekago = weekago.reduce((acc, a) => acc + +a['deaths'], 0) 
+                document.querySelector('#confWeekago').innerHTML = change(confCurrent, confWeekago) + '% (+ ' + diff(confCurrent, confWeekago) + ')'
+                document.querySelector('#diedWeekago').innerHTML = change(diedCurrent, diedWeekago) + '% (+ ' + diff(diedCurrent, diedWeekago) + ')'
+            })
+        
+        // Get a month ago data
+        let dateMonthago = calcDaysAgo(dateCurrent, 30)
+        fetch(`https://covid19.mathdro.id/api/daily/${dateMonthago}`)
+            .then(res => res.json())
+            .then(monthago => {
+                monthago = monthago.filter(a => a.countryRegion === location)
+                let confMonthago = monthago.reduce((acc, a) => acc + +a['confirmed'], 0) 
+                let diedMonthago = monthago.reduce((acc, a) => acc + +a['deaths'], 0) 
+                document.querySelector('#confMonthago').innerHTML = change(confCurrent, confMonthago) + '% (+ ' + diff(confCurrent, confMonthago) + ')'
+                document.querySelector('#diedMonthago').innerHTML = change(diedCurrent, diedMonthago) + '% (+ ' + diff(diedCurrent, diedMonthago) + ')'
+            })
+        
+        // Get a month ago data
+        let dateQuarterago = calcDaysAgo(dateCurrent, 90)
+        fetch(`https://covid19.mathdro.id/api/daily/${dateQuarterago}`)
+            .then(res => res.json())
+            .then(quarterago => {
+                quarterago = quarterago.filter(a => a.countryRegion === location)
+                let confQuarterago = quarterago.reduce((acc, a) => acc + +a['confirmed'], 0) 
+                let diedQuarterago = quarterago.reduce((acc, a) => acc + +a['deaths'], 0) 
+                document.querySelector('#confQuarterago').innerHTML = change(confCurrent, confQuarterago) + '% (+ ' + diff(confCurrent, confQuarterago) + ')'
+                document.querySelector('#diedQuarterago').innerHTML = change(diedCurrent, diedQuarterago) + '% (+ ' + diff(diedCurrent, diedQuarterago) + ')'
+            })
+    })
+
+})
